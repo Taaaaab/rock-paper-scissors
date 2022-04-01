1da66318212e7d84722e1315f74543f01b463ccc
Taaaaab <owen.whitsett@gmail.com>
2022-03-31 21:46:35 -0400

    Add round and static player selection

--- script.js
+++ script.js
@@ -25,3 +25,31 @@ function computerPlay() {
 }
 
 computerPlay()
+
+let playerSelection = (1);
+
+console.log("Must input 0, 1 or 2");
+console.log(playerSelection);
+
+let computerSelection = randomNum;
+
+function playRound() {
+    if (playerSelection === computerSelection)
+    console.log("Draw, play again");
+    else if (playerSelection === 0 && computerSelection === 1)
+    console.log("You Lose");
+    else if (playerSelection === 0 && computerSelection === 2)
+    console.log("You Win");
+    else if (playerSelection === 1 && computerSelection === 0)
+    console.log("You Win");
+    else if (playerSelection === 1 && computerSelection === 2)
+    console.log("You Lose");
+    else if (playerSelection === 2 && computerSelection === 0)
+    console.log("You Lose");
+    else if (playerSelection === 2 && computerSelection === 1)
+    console.log("You Win");
+    else;
+    console.log();
+}
+
+playRound(playerSelection, computerSelection);
