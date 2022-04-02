2310007dad3bd288752dd9aa9becbe3cdb6ed755
Taaaaab <owen.whitsett@gmail.com>
2022-04-02 12:14:14 -0400

    Add working player selection

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 // Rock Paper Scissors
 // 3 choices with different strengths and weaknesses
-// rock > scissors
-// paper > rock
-// scissors > paper
 // game against computer
+// computer picks using random number from 0-2
+// player enters choice which is stored as a variable
+// function runs game using computer and player choices
 
-let rock = (0)
-let paper = (1)
-let scissors = (3)
-let randomNum = Math.floor(Math.random() * 3)
-
+const randomNum = Math.floor(Math.random() * 3)
 console.log(randomNum);
 
 function computerPlay() {
@@ -21,33 +17,46 @@ function computerPlay() {
     console.log("Scissors");
     else
     console.log("");
- 
+
 }
 
 computerPlay()
 
-let playerSelection = (1);
+// have player input choice 
+let selection = prompt("Rock, Paper or Scissors?" , "Rock") 
+let playerSelection = selection.toLowerCase();
 
-console.log("Must input 0, 1 or 2");
+// convert name to number
+let rock = (0);
+let paper = (1);
+let scissors = (2);
+if (playerSelection === "rock")
+playerSelection = rock;
+else if (playerSelection === "paper")
+playerSelection = paper;
+else if (playerSelection === "scissors")
+playerSelection = scissors;
+
 console.log(playerSelection);
 
+// play round using player input and computer random number
 let computerSelection = randomNum;
 
 function playRound() {
     if (playerSelection === computerSelection)
     console.log("Draw, play again");
     else if (playerSelection === 0 && computerSelection === 1)
-    console.log("You Lose");
+    console.log("You Lose! Paper beats Rock");
     else if (playerSelection === 0 && computerSelection === 2)
-    console.log("You Win");
+    console.log("You Win! Rock beats Scissors");
     else if (playerSelection === 1 && computerSelection === 0)
-    console.log("You Win");
+    console.log("You Win! Paper beats Rock");
     else if (playerSelection === 1 && computerSelection === 2)
-    console.log("You Lose");
+    console.log("You Lose! Scissors beats Paper");
     else if (playerSelection === 2 && computerSelection === 0)
-    console.log("You Lose");
+    console.log("You Lose! Rock beats Scissors");
     else if (playerSelection === 2 && computerSelection === 1)
-    console.log("You Win");
+    console.log("You Win! Scissors beats Paper");
     else;
     console.log();
 }
